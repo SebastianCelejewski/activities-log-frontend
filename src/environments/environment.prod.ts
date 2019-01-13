c879a1a560b7e67759feb75f90db7bc6ae0b26c5
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2019-01-13 12:34:17 +0100

    Updating config for test and prod environments as a preparation for first deployment of Duties functionality to Test environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     name: 'prod',
     production: true,
-    apiUrl: 'https://c4mpyiczgd.execute-api.eu-central-1.amazonaws.com/prod/activities',
+    apiUrl: 'https://c4mpyiczgd.execute-api.eu-central-1.amazonaws.com/prod',
     cognitoUserPool: '',
     cognitoClientId: ''
 };
--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,7 +1,7 @@
 export const environment = {
     name: 'test',
-    production: true,
-    apiUrl: 'https://c4mpyiczgd.execute-api.eu-central-1.amazonaws.com/test/activities',
+    production: false,
+    apiUrl: 'https://c4mpyiczgd.execute-api.eu-central-1.amazonaws.com/test',
     cognitoUserPool: 'eu-central-1_88sJxaawH',
     cognitoClientId: '17si7vju0rc2ettrerbbmsahm'
 };
