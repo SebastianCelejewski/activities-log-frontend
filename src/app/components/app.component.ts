e7fd556678bc218fe4deb44474272d7eeaaee643
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2019-01-20 13:07:43 +0100

    An attemtp to make unit tests running

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -7,6 +7,6 @@ import { environment } from '../../environments/environment';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'Activities Log';
+    title = 'Activity Log';
     envName = environment.name;
 }
